a81af065425decca64a17d71b5609ca81cde4bc1
Furkan ŞAHİN <furkansahin_fs@outlook.com>
2020-05-24 01:06:57 +0300

    son

--- Business Application/src/Exceptions/DifferentUncountableItemsException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-
-@SuppressWarnings("serial")
-public class DifferentUncountableItemsException extends Exception { 
-	   
-	public DifferentUncountableItemsException(String errorMessage) {
-	        super(errorMessage);
-	    }
-	}
--- Business Application/src/Exceptions/ItemNotCountableException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-
-@SuppressWarnings("serial")
-public class ItemNotCountableException extends Exception{
-	public ItemNotCountableException(String errorMessage) {
-		super(errorMessage);
-	}
-
-}
